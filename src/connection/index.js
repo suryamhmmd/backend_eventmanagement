f39cf08f8c54b68e6867caa1ede5909379e9c03c
suryamhmmd <suryamhmmd@users.noreply.github.com>
2020-05-03 20:27:28 +0700

    proposal backend

--- src/connection/index.js
+++ src/connection/index.js
@@ -2,7 +2,7 @@ const mysql = require('mysql')
 
 const conn = mysql.createConnection({
     user: "root",
-    password: "Ihsanazmi26",
+    password: "",
     host: "localhost",
     database: "event_management",
     port: 3306
--- src/routers/suratRouter.js
+++ src/routers/suratRouter.js
@@ -39,4 +39,41 @@ router.put(`/save_surat/:id_surat`, (req, res)=>{
 })
 
 
+//ROUTER PROPOSAL
+router.post('/proposal', (req, res)=>{
+    let sql = `INSERT INTO proposal SET ?`
+    let data = req.body
+    
+    conn.query(sql, data, (err, result)=>{
+        if(err) return res.send({error:err.message})
+        res.send(result)
+    })
+})
+
+router.get('/proposal/:id_event', (req, res)=>{
+    let sql = `SELECT * from proposal where id_event = '${req.params.id_event}'`
+
+    conn.query(sql, (err, result)=>{
+        if(err) return res.send({error: err.message})
+        res.send(result)
+    })
+})
+
+router.get('/edit_proposal/:id_proposal', (req, res)=>{
+    let sql = `SELECT * from proposal where id_proposal = '${req.params.id_proposal}'`
+    conn.query(sql, (err, result)=>{
+        if(err) return res.send({error: err.message})
+        res.send(result)
+    })
+})
+
+router.put(`/save_proposal/:id_proposal`, (req, res)=>{
+    let sql = `UPDATE proposal SET ? WHERE id_proposal= ${req.params.id_proposal}`
+    let data = req.body
+
+    conn.query(sql, data, (err, result)=>{
+        if(err) return res.send({error: err.message})
+        res.send(result)
+    })
+})
 module.exports = router
